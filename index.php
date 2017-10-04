a848f1e0bb0d023eafdc4fd82e73118b68430315
yw674 <yw674@njit.edu>
2017-10-04 11:16:03 -0400

    Fixed syntax errors (why did I try to be fancy with a boolean type\?). Final commit.

--- index.php
+++ index.php
@@ -184,11 +184,7 @@
 	
 	class logic {
 		static public function isLeapYear($year){
-			$bool = bool(False);
-			if (year is not divisible by 4) then (it is a common year)
-			else if (year is not divisible by 100) then (it is a leap year)
-			else if (year is not divisible by 400) then (it is a common year)
-			else (it is a leap year)
+			$bool = 'False';
 			
 			switch ($year % 4) {
 				case 0: 
